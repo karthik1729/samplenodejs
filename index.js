f411608c33bf41f766de41e0e6842bec0082e684
Karthik Th <karthik@kloudlite.io>
2024-02-15 15:38:41 +0530

    send env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(JSON.stringify(process.env));
 });
 
 app.listen(3000, () => {
